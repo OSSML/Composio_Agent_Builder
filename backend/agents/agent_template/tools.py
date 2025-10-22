0a875edeafee696ddcbafe36d9c9f2f3cf29a460
arthiondaena <arthiondaena@gmail.com>
2025-10-22 17:51:27 +0530

    fix: Removed unnecessary stuff

--- backend/agents/agent_template/tools.py
+++ backend/agents/agent_template/tools.py
@@ -6,14 +6,3 @@ def fetch_tools(tools: list):
     composio = Composio(provider=LanggraphProvider())
     tools = composio.tools.get("hey@example.com", tools=tools)
     return tools
-
-
-if __name__ == "__main__":
-    from dotenv import load_dotenv
-
-    load_dotenv()
-    # toolkits = ["GOOGLEDOCS"]
-    # output = asyncio.run(fetch_tools(["googledocs"], ["GITHUB_FIND_PULL_REQUESTS", "GOOGLEDOCS_CREATE_DOCUMENT_MARKDOWN"]))
-    # print(output)
-    # import json
-    # print(json.dumps(output, indent=4))
